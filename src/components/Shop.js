818977162f4115836057c8498fbdf7aa7be26c49
melissawu2022 <mwu070@uottawa.ca>
2021-03-01 21:03:16 -0500

    made grid thing work :)

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Paper from "@material-ui/core/Paper";
+import "./SpacingGrid.css";
 import "./Shop.css";
 
 class Shop extends Component {
     render() {
-        const { name, rating, image_url } = this.props.shop;
+        const { name, rating, image_url, location } = this.props.shop;
         return (
             <Paper className="paper-style">
-                <h1 className="spacing-grid-paper-h1"> {name} </h1>
-                <h2 className="spacing-grid-paper-h2">Rating: {rating}</h2>
-                <img className="bbt-image" src={image_url} />
+                <h1 className="first-header"> {name} </h1>
+                <h2 className="second-header">Rating: {rating}</h2>
+                <img className="bbt-image" src={image_url} alt=" " />
             </Paper>
         );
     }
--- src/components/SpacingGrid.js
+++ src/components/SpacingGrid.js
@@ -1,40 +1,13 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
 import "./SpacingGrid.css";
 import Shop from "./Shop";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1
-    },
-    paper: {
-        height: 200,
-        width: 200
-    },
-    control: {
-        padding: theme.spacing(2)
-    }
-}));
-
 export default function SpacingGrid(props) {
-    const [spacing] = React.useState(2);
-    const classes = useStyles();
-
     return (
-        <Grid container className={classes.root} spacing={2}>
-            <Grid item xs={12}>
-                <Grid container justify="center" spacing={spacing}>
-                    {[0].map((idx) => (
-                        <Grid key={idx} item>
-                            
-                            {props.shopList.map((shop, idx2) => (
-                                <Shop shop={shop} key={idx2} />
-                            ))}
-                        </Grid>
-                    ))}
-                </Grid>
-            </Grid>
-        </Grid>
+        <div class="grid-container">
+            {props.shopList.map((shop, idx2) => (
+                <Shop shop={shop} key={idx2} />
+            ))}
+        </div>
     );
 }
